0974fcfc7025044869a9e7e57dbcf7138820e76f
Jeremy Beck <jeremycbeck@outlook.com>
2018-10-23 09:24:58 -0700

    create folder scaffold for project; added Person class and HomeController

--- lab11_mvcApp/lab11_mvcApp/Startup.cs
+++ lab11_mvcApp/lab11_mvcApp/Startup.cs
@@ -15,6 +15,7 @@ namespace lab11_mvcApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +25,15 @@ namespace lab11_mvcApp
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}");
+            });
 
             app.Run(async (context) =>
             {
